2f9c9e6ce028f10b5f863ebb071da4f92ebc2131
alena <aleka-mina@mail.ru>
2019-04-22 18:05:35 +0300

    add score logging

--- snake/genetic.py
+++ snake/genetic.py
@@ -5,8 +5,8 @@ def cal_pop_fitness(pop, screen, file):
     # calculating the fitness value by playing a game with the given weights in chromosome
     fitness = []
     for i in range(pop.shape[0]):
-        fit = run_game(pop[i], screen)
-        file.write('fitness value of chromosome '+ str(i) +' :  ' +str(fit) + '\n')
+        score, fit = run_game(pop[i], screen)
+        file.write('fitness value of chromosome '+ str(i) +' :  ' +str(fit) +' :  score' +str(score)+ '\n')
         fitness.append(fit)
     return np.array(fitness)
 
